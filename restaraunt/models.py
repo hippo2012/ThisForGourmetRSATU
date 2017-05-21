a5afb0c7d541b77d889a2a1d72b1b5cd686530f4
Ilya Obolensky <ilya03601@mail.ru>
2017-05-21 20:17:55 +0300

    Корректное отображение названий полей в адмнке

--- restaraunt/models.py
+++ restaraunt/models.py
@@ -1,8 +1,25 @@
+# -*-coding: utf-8 -*-
 from django.db import models
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255, default='', null=False, blank=False)
 
+    class Meta:
+        verbose_name = u'Ингредиент'
+        verbose_name_plural = u'Ингредиенты'
+
+    def __str__(self):
+        return self.name.encode('utf-8')
+
+
 class Dish(models.Model):
-    name=models.CharField(max_length=255, default='', null=False, blank=False)
+    name = models.CharField(max_length=255, default='', null=False, blank=False)
     ingredient = models.ManyToManyField(Ingredient)
+
+    class Meta:
+        verbose_name = u'Блюдо'
+        verbose_name_plural = u'Блюда'
+
+    def __str__(self):
+        return self.name.encode('utf-8')
