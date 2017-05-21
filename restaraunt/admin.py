7c11b21f50c9ce586da7fbd645ea7e2fbdaa0b8f
IVANLAPTOP\Programmer <hippo2012@gmail.com>
2017-05-21 13:47:35 +0300

    + ингредиенты

--- restaraunt/admin.py
+++ restaraunt/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Dish)
+admin.site.register(models.Ingredient)
--- restaraunt/models.py
+++ restaraunt/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, default='', null=False, blank=False)
+
 class Dish(models.Model):
     name=models.CharField(max_length=255, default='', null=False, blank=False)
+    ingredient = models.ManyToManyField(Ingredient)
