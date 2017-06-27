ba541a3c8975cf7a6f3e7bdc221cebe32d2b2428
Ilya Obolensky <ilya@astroshock.ru>
2017-06-27 20:54:23 +0300

    График наличия ингредиентов на складе.

--- control/urls.py
+++ control/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^ingredients/$', views.IngredientsView.as_view(), name='ingredients'),
     url(r'^orders/(?P<id>\d+)/$', views.OrdersView.as_view(), name='order'),
+    url(r'^storage/$', views.StorageView.as_view(), name='storage'),
     url(r'^api/', include('control.api_urls', app_name='control', namespace='api'))
 ]
--- control/views.py
+++ control/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import TemplateView, View
 from django.utils import timezone
+
+import plotly.graph_objs as go
+import plotly.offline as opy
+
 from restaraunt import models
 
 
@@ -54,5 +58,33 @@ class OrdersView(TemplateView):
             dishes = models.Order.objects.filter(date=timezone.now() + timezone.timedelta(days=1))
 
         context['dishes'] = [models.Dish.objects.get(id=d.dish_id) for d in dishes]
+
+        return context
+
+class StorageView(TemplateView):
+    template_name = "control/Storage.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(StorageView, self).get_context_data(**kwargs)
+
+        labels = []
+        values = []
+        for ingr in models.Ingredient.objects.all():
+            labels.append(ingr.name)
+            values.append(0)
+
+        for item in models.Storage.objects.all():
+            ingr = models.Ingredient.objects.get(id=item.ingredient_id)
+            for i in range(len(labels)):
+                if labels[i] == ingr.name:
+                    values[i] += item.count
+
+        trace = go.Pie(labels=labels, values=values)
+
+        data=go.Data([trace])
+        figure=go.Figure(data=data)
+        div = opy.plot(figure, auto_open=False, output_type='div')
+
+        context['graph'] = div
 
         return context
